fa6036e434dd0bfb541d19a3f5940529ac80c870
David Anderson <dave@natulte.net>
2009-12-30 00:59:57 +0000

    libcpu: Cast struct timespec fields to uint64_t to avoid overflow.

--- libcpu/timings.cpp
+++ libcpu/timings.cpp
@@ -15,7 +15,7 @@ uint64_t abs_time() {
   struct timespec ts;
   if (clock_gettime(CLOCK_MONOTONIC, &ts) < 0)
     return 0;
-  return ts.tv_sec * 1000000000 + ts.tv_nsec;
+  return (uint64_t)ts.tv_sec * 1000000000 + (uint64_t)ts.tv_nsec;
 }
 
 #endif
