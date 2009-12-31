8438b676626701ca7b00989622a243e29dc16fe9
Pekka Enberg <penberg@kernel.org>
2009-12-31 07:48:06 +0000

    x86: Simple test case for i8086
    This patch adds a "test/bin/x86/i8086.bin" which contains the following i8086
    machine code:
    
      # i8086.S
        .code16gcc
        .text
        .globl  _start
        .type   _start, @function
      _start:
        movw $1, %ax
        movw $2, %bx
        movw $3, %cx
        movw $4, %dx
        retw
    
    that was compiled into a binary with the following commands:
    
      gcc -nostdinc -c i8086.S -o i8086.o
      ld -nostdlib -static i8086.o -o i8086.elf
      objcopy -O binary i8086.elf i8086.bin
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

---
+++ test/scripts/8086.sh
@@ -0,0 +1,1 @@
+./build/libcpu/test_run86 test/bin/x86/i8086.bin
---
+++ test/x86/run.cpp
@@ -0,0 +1,92 @@
+#define START 0
+#define ENTRY 0
+
+#include "timings.h"
+
+#include <libcpu.h>
+#include "arch/mips/libcpu_mips.h"
+#include "arch/arm/libcpu_arm.h"
+#include "arch/m88k/libcpu_m88k.h"
+#include "arch/m88k/m88k_isa.h"
+#include "arch/x86/libcpu_8086.h"
+
+#define RET_MAGIC 0x4D495354
+
+static void
+debug_function(cpu_t *cpu)
+{
+	fprintf(stderr, "%s:%u\n", __FILE__, __LINE__);
+}
+
+//////////////////////////////////////////////////////////////////////
+#define SINGLESTEP_NONE	0
+#define SINGLESTEP_STEP	1
+#define SINGLESTEP_BB	2
+//////////////////////////////////////////////////////////////////////
+
+int
+main(int argc, char **argv)
+{
+	char *s_arch;
+	char *executable;
+	cpu_arch_t arch;
+	cpu_t *cpu;
+	uint8_t *RAM;
+	FILE *f;
+	int ramsize;
+	int r1, r2;
+
+	int singlestep = SINGLESTEP_NONE;
+	int log = 1;
+	int print_ir = 1;
+
+	/* parameter parsing */
+	if (argc < 2) {
+		printf("Usage: %s [binary]\n", argv[0]);
+		return 0;
+	}
+	executable = argv[1];
+	arch = CPU_ARCH_8086;
+
+	ramsize = 1*1024*1024;
+	RAM = (uint8_t*)malloc(ramsize);
+
+	cpu = cpu_new(arch, 0, 0);
+
+	cpu_set_flags_optimize(cpu, CPU_OPTIMIZE_ALL);
+	cpu_set_flags_debug(cpu, 0
+		| (print_ir? CPU_DEBUG_PRINT_IR : 0)
+		| (print_ir? CPU_DEBUG_PRINT_IR_OPTIMIZED : 0)
+		| (log? CPU_DEBUG_LOG :0)
+		| (singlestep == SINGLESTEP_STEP? CPU_DEBUG_SINGLESTEP    : 0)
+		| (singlestep == SINGLESTEP_BB?   CPU_DEBUG_SINGLESTEP_BB : 0)
+		);
+
+	cpu_set_ram(cpu, RAM);
+	
+	/* load code */
+	if (!(f = fopen(executable, "rb"))) {
+		printf("Could not open %s!\n", executable);
+		return 2;
+	}
+	cpu->code_start = START;
+	cpu->code_end = cpu->code_start + fread(&RAM[cpu->code_start], 1, ramsize-cpu->code_start, f);
+	fclose(f);
+	cpu->code_entry = cpu->code_start + ENTRY;
+
+	cpu_tag(cpu, cpu->code_entry);
+
+	cpu_translate(cpu); /* force translation now */
+
+	printf("\n*** Executing...\n");
+
+	printf("GUEST run..."); fflush(stdout);
+
+	cpu_run(cpu, debug_function);
+
+	printf("done!\n");
+
+	cpu_free(cpu);
+
+	return 0;
+}
