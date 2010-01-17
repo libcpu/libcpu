5f5ac278d28f16a3b6ebd0f269648c6021706a73
Tilmann Scheller <tilmann.scheller@googlemail.com>
2010-01-17 17:23:17 +0000

    Add preliminary disassembler.

--- arch/fapra/fapra_internal.h
+++ arch/fapra/fapra_internal.h
@@ -5,4 +5,39 @@ int arch_fapra_disasm_instr(cpu_t *cpu, addr_t pc, char *line, unsigned int max_
 int arch_fapra_translate_instr(cpu_t *cpu, addr_t pc, BasicBlock *bb);
 Value *arch_fapra_translate_cond(cpu_t *cpu, addr_t pc, BasicBlock *bb);
 
-#define INSTR(a) RAM32(cpu->RAM, a)
+#define INSTR(a) RAM32LE(cpu->RAM, a)
+
+enum {
+  LDW = 0x10,
+  STW = 0x11,
+  LDB = 0x1C,
+  STB = 0x1D,
+
+  LDIH = 0x20,
+  LDIL = 0x21,
+
+  JMP = 0x30,
+  BRA = 0x34,
+  BZ = 0x35,
+  BNZ = 0x36,
+  NOP = 0x32,
+  CALL = 0x33,
+  BL = 0x37,
+  RFE = 0x3F,
+
+  ADDI = 0x0F,
+  ADD = 0x00,
+  SUB = 0x01,
+  AND = 0x02,
+  OR = 0x03,
+  NOT = 0x05,
+  SARI = 0x0B,
+  SAL = 0x06,
+  SAR = 0x07,
+  MUL = 0x08,
+
+  PERM = 0x09,
+  RDC8 = 0x0A,
+  TGE = 0x0C,
+  TSE = 0x0D
+};
--- arch/fapra/fapra_translate.cpp
+++ arch/fapra/fapra_translate.cpp
@@ -13,41 +13,6 @@ using namespace llvm;
 // fapra: instruction decoding
 //////////////////////////////////////////////////////////////////////
 
-enum {
-  LDW = 0x10,
-  STW = 0x11,
-  LDB = 0x1C,
-  STB = 0x1D,
-
-  LDIH = 0x20,
-  LDIL = 0x21,
-
-  JMP = 0x30,
-  BRA = 0x34,
-  BZ = 0x35,
-  BNZ = 0x36,
-  NOP = 0x32,
-  CALL = 0x33,
-  BL = 0x37,
-  RFE = 0x3F,
-
-  ADDI = 0x0F,
-  ADD = 0x00,
-  SUB = 0x01,
-  AND = 0x02,
-  OR = 0x03,
-  NOT = 0x05,
-  SARI = 0x0B,
-  SAL = 0x06,
-  SAR = 0x07,
-  MUL = 0x08,
-
-  PERM = 0x09,
-  RDC8 = 0x0A,
-  TGE = 0x0C,
-  TSE = 0x0D
-};
-
 // Instruction decoding helper functions.
 static inline uint32_t opc(uint32_t ins) {
   return ins >> 26;
