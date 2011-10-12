793f2cde235cce1fd5d43e93e61a4c05faece0c0
Pekka Enberg <penberg@kernel.org>
2011-10-12 23:38:33 +0300

    Fix type conflict in libcpu/types.h on 64-bit Linux
    This patch is a quick and dirty fix for the following build issue on 64-bit
    Linux:
    
      In file included from /home/penberg/libcpu/libcpu/libcpu.h:23:0,
                       from /home/penberg/libcpu/arch/arm/arm_arch.cpp:9:
      /home/penberg/libcpu/libcpu/types.h:16:28: error: conflicting declaration ‘typedef long long unsigned int uint64_t’
      /usr/include/stdint.h:56:27: error: ‘uint64_t’ has a previous declaration as ‘typedef long unsigned int uint64_t’
      make[3]: *** [arch/arm/CMakeFiles/arm.dir/arm_arch.cpp.o] Error 1
      In file included from /home/penberg/libcpu/libcpu/libcpu.h:23:0,
                       from /home/penberg/libcpu/arch/arm/arm_translate.cpp:9:
      /home/penberg/libcpu/libcpu/types.h:16:28: error: conflicting declaration ‘typedef long long unsigned int uint64_t’
      /usr/include/stdint.h:56:27: error: ‘uint64_t’ has a previous declaration as ‘typedef long unsigned int uint64_t’
    
    Proper fix is to use <stdint.h> instead of redefining uint64_t ourselves.
    
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- libcpu/types.h
+++ libcpu/types.h
@@ -9,7 +9,7 @@ typedef signed short sint16_t;
 typedef signed int sint32_t;
 typedef signed long long sint64_t;
 
-#ifndef sun
+#if !defined(sun) && !defined(linux)
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
