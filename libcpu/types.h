5970173e5579969fd03d5799dac95aa32f8b8d50
Ryan Govostes <rgovostes@gmail.com>
2012-04-08 00:18:41 -0700

    New LLVM MC-based disassembler.

--- libcpu/types.h
+++ libcpu/types.h
@@ -24,7 +24,7 @@ typedef uint16_t tag_t;
 #include <arch/m68k/m68k_types.h>
 #include <arch/mips/mips_types.h>
 #include <arch/x86/x86_types.h>
-#include <arch/i386/i386_types.h>
+#include <arch/i386/i386_regfile.h>
 #include <arch/fapra/fapra_types.h>
 
 #endif
