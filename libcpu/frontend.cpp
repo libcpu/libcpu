18959440b012b31a19c28003322c7c9e37e55f2b
Oliver Pinter <oliver.pntr@gmail.com>
2013-08-23 14:23:32 +0200

    fix build
    [ 18%] Building CXX object libcpu/CMakeFiles/cpu.dir/frontend.cpp.o
    /home/op/work/ukatemi/libcpu/libcpu/frontend.cpp:455:40: error: no member named 'getTargetData' in 'llvm::ExecutionEngine'
            Type *intptr_type = cpu->exec_engine->getTargetData()->getIntPtrType(_CTX());
                                ~~~~~~~~~~~~~~~~  ^
    1 error generated.
    *** [libcpu/CMakeFiles/cpu.dir/frontend.cpp.o] Error code 1
    
    Stop in /usr/home/op/work/ukatemi/libcpu.
    *** [libcpu/CMakeFiles/cpu.dir/all] Error code 1
    
    Stop in /usr/home/op/work/ukatemi/libcpu.
    *** [all] Error code 1

--- libcpu/frontend.cpp
+++ libcpu/frontend.cpp
@@ -452,7 +452,7 @@ arch_debug_me(cpu_t *cpu, BasicBlock *bb)
 	if (cpu->ptr_func_debug == NULL)
 		return;
 
-	Type *intptr_type = cpu->exec_engine->getTargetData()->getIntPtrType(_CTX());
+	Type *intptr_type = cpu->exec_engine->getDataLayout()->getIntPtrType(_CTX());
 	Constant *v_cpu = ConstantInt::get(intptr_type, (uintptr_t)cpu);
 	Value *v_cpu_ptr = ConstantExpr::getIntToPtr(v_cpu, PointerType::getUnqual(intptr_type));
 
