88c4667acbe1e796cfa1184fa6d4eff963c6f84b
Pekka Enberg <penberg@iki.fi>
2014-01-08 19:14:39 +0200

    Fix LLVM includes in interface.cpp
    Signed-off-by: Pekka Enberg <penberg@iki.fi>

--- libcpu/interface.cpp
+++ libcpu/interface.cpp
@@ -8,12 +8,11 @@
 #include <string.h>
 #include <inttypes.h>
 
-#include "llvm/IR/Analysis/Verifier.h"
-#include "llvm/IR/ExecutionEngine/JIT.h"
-#include "llvm/IR/LinkAllPasses.h"
+#include "llvm/Analysis/Verifier.h"
+#include "llvm/ExecutionEngine/JIT.h"
+#include "llvm/LinkAllPasses.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/Target/TargetData.h"
-#include "llvm/IR/Target/TargetSelect.h"
+#include "llvm/Support/TargetSelect.h"
 
 /* project global headers */
 #include "libcpu.h"
