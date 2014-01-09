libcpu/libcpu

commit 13da3c78e8ebae371234851cbee62ef1eeeced0b

x86: Conditional jump instructions
Signed-off-by: Pekka Enberg <penberg@iki.fi>

arch/x86/x86_decode.cpp
@@ -15,6 +15,8 @@
 
 #define INSTR_UNDEFINED		0
 
+#define Jb ADDMODE_REL | WIDTH_BYTE
+
 static const uint32_t decode_table[256] = {
 	/*[0x0]*/	INSTR_ADD | ADDMODE_REG_RM | WIDTH_BYTE,
 	/*[0x1]*/	INSTR_ADD | ADDMODE_REG_RM | WIDTH_FULL,
@@ -128,22 +130,22 @@ static const uint32_t decode_table[256] = {
 	/*[0x6D]*/	INSTR_UNDEFINED,
 	/*[0x6E]*/	INSTR_UNDEFINED,
 	/*[0x6F]*/	INSTR_UNDEFINED,
-	/*[0x70]*/	0,
-	/*[0x71]*/	0,
-	/*[0x72]*/	0,
-	/*[0x73]*/	0,
-	/*[0x74]*/	0,
-	/*[0x75]*/	0,
-	/*[0x76]*/	0,
-	/*[0x77]*/	0,
-	/*[0x78]*/	0,
-	/*[0x79]*/	0,
-	/*[0x7A]*/	0,
-	/*[0x7B]*/	0,
-	/*[0x7C]*/	0,
-	/*[0x7D]*/	0,
-	/*[0x7E]*/	0,
-	/*[0x7F]*/	0,
+	/*[0x70]*/	INSTR_JO  | Jb,
+	/*[0x71]*/	INSTR_JNO | Jb,
+	/*[0x72]*/	INSTR_JB  | Jb,
+	/*[0x73]*/	INSTR_JNB | Jb,
+	/*[0x74]*/	INSTR_JZ  | Jb,
+	/*[0x75]*/	INSTR_JNZ | Jb,
+	/*[0x76]*/	INSTR_JBE | Jb,
+	/*[0x77]*/	INSTR_JA  | Jb,
+	/*[0x78]*/	INSTR_JS  | Jb,
+	/*[0x79]*/	INSTR_JNS | Jb,
+	/*[0x7A]*/	INSTR_JPE | Jb,
+	/*[0x7B]*/	INSTR_JPO | Jb,
+	/*[0x7C]*/	INSTR_JL  | Jb,
+	/*[0x7D]*/	INSTR_JGE | Jb,
+	/*[0x7E]*/	INSTR_JLE | Jb,
+	/*[0x7F]*/	INSTR_JG  | Jb,
 	/*[0x80]*/	0,
 	/*[0x81]*/	0,
 	/*[0x82]*/	0,
@@ -421,7 +423,7 @@ decode_imm_byte(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
 }
 
 static void
-decode_imm(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
+decode_imm_rel(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
 {
 	if (instr->flags & SRC_IMM8) {
 		decode_imm_byte(instr, RAM, pc);
@@ -436,15 +438,6 @@ decode_imm(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
 		decode_imm_byte(instr, RAM, pc);
 		break;
 	}
-}
-
-static void
-decode_imm_rel(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
-{
-	if (instr->flags & IMM_MASK)
-		decode_imm(instr, RAM, pc);
-	else
-		decode_rel(instr, RAM, pc);
 }
 
 static void

test/bin/x86/i8086/i8086.S
@@ -297,6 +297,25 @@ _start:
 	popw %ss
 	popw %ds
 
+	# 0x70
+	jo  L1
+	jno L1
+	jb  L1
+	jnb L1
+	jz  L1
+	jnz L1
+	jbe L1
+	ja  L1
+	js  L1
+	jns L1
+	jpe L1
+	jpo L1
+	jl  L1
+	jge L1
+	jle L1
+	jg  L1
+L1:
+
 	# 0xb80+rw ("mov")
 	movw $1, %ax
 	movw $1, %cx

commit 1eddf2cf641cb7fe72095e766983e982da16cf02

x86: Fix relative address decoding
Commit 13da3c7 ("x86: Conditional jump instructions") broke decoding of
negative relative addresses.

Fix that up.

Signed-off-by: Pekka Enberg <penberg@iki.fi>

arch/x86/x86_decode.cpp
@@ -384,59 +384,88 @@ decode_src_operand(struct x86_instr *instr)
 	}
 }
 
-static void
-decode_rel(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
+static uint8_t read_u8(uint8_t* RAM, addr_t *pc)
 {
 	addr_t new_pc = *pc;
 
-	uint8_t imm_lo = RAM[new_pc++];
-	uint8_t imm_hi = RAM[new_pc++];
-
-	instr->imm_data	= (int16_t)((imm_hi << 8) | imm_lo);
-	instr->nr_bytes	+= 2;
+	uint8_t ret = (uint8_t)RAM[new_pc++];
 
 	*pc = new_pc;
+
+	return ret;
 }
 
-static void
-decode_imm_full(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
+static uint8_t read_s8(uint8_t* RAM, addr_t *pc)
 {
 	addr_t new_pc = *pc;
 
-	uint8_t imm_lo = RAM[new_pc++];
-	uint8_t imm_hi = RAM[new_pc++];
-
-	instr->imm_data	= (uint16_t)((imm_hi << 8) | imm_lo);
-	instr->nr_bytes	+= 2;
+	uint8_t ret = (uint8_t)RAM[new_pc++];
 
 	*pc = new_pc;
+
+	return ret;
 }
 
-static void
-decode_imm_byte(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
+static uint16_t read_u16(uint8_t* RAM, addr_t *pc)
 {
 	addr_t new_pc = *pc;
 
-	instr->imm_data	= (uint8_t)RAM[new_pc++];
-	instr->nr_bytes	+= 1;
+	uint8_t lo = RAM[new_pc++];
+	uint8_t hi = RAM[new_pc++];
+
+	uint16_t ret = (uint16_t)((hi << 8) | lo);
 
 	*pc = new_pc;
+
+	return ret;
+}
+
+static int16_t read_s16(uint8_t* RAM, addr_t *pc)
+{
+	addr_t new_pc = *pc;
+
+	uint8_t lo = RAM[new_pc++];
+	uint8_t hi = RAM[new_pc++];
+
+	int16_t ret = (int16_t)((hi << 8) | lo);
+
+	*pc = new_pc;
+
+	return ret;
 }
 
 static void
-decode_imm_rel(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
+decode_imm(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
 {
 	if (instr->flags & SRC_IMM8) {
-		decode_imm_byte(instr, RAM, pc);
+		instr->imm_data = read_u8(RAM, pc);
+		instr->nr_bytes += 1;
 		return;
 	}
 
 	switch (instr->flags & WIDTH_MASK) {
 	case WIDTH_FULL:
-		decode_imm_full(instr, RAM, pc);
+		instr->imm_data = read_u16(RAM, pc);
+		instr->nr_bytes += 2;
 		break;
 	case WIDTH_BYTE:
-		decode_imm_byte(instr, RAM, pc);
+		instr->imm_data = read_u8(RAM, pc);
+		instr->nr_bytes += 1;
+		break;
+	}
+}
+
+static void
+decode_rel(struct x86_instr *instr, uint8_t* RAM, addr_t *pc)
+{
+	switch (instr->flags & WIDTH_MASK) {
+	case WIDTH_FULL:
+		instr->imm_data = read_s16(RAM, pc);
+		instr->nr_bytes += 2;
+		break;
+	case WIDTH_BYTE:
+		instr->imm_data = read_s8(RAM, pc);
+		instr->nr_bytes += 1;
 		break;
 	}
 }
@@ -565,8 +594,11 @@ done_prefixes:
 	if (instr->flags & MEM_DISP_MASK)
 		decode_disp(instr, RAM, &pc);
 
-	if (instr->flags & (IMM_MASK|REL_MASK))
-		decode_imm_rel(instr, RAM, &pc);
+	if (instr->flags & IMM_MASK)
+		decode_imm(instr, RAM, &pc);
+
+	if (instr->flags & REL_MASK)
+		decode_rel(instr, RAM, &pc);
 
 	decode_src_operand(instr);
 